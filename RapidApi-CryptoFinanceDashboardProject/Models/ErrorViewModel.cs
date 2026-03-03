eb99b61a417c97b06785294c5c7ddf7e5befcbd5
tunadeveloper <tunabusiness25@gmail.com>
2026-03-03 13:04:05 +0300

    Add project files.

---
+++ RapidApi-CryptoFinanceDashboardProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RapidApi_CryptoFinanceDashboardProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
